476d400c8a782900da4767a60e08e911ac3b4c49
Unknown <b6010572@my.shu.ac.uk>
2017-12-02 10:19:25 +0000

    More node trouble (water)

--- source/scripts/ocean.gd
+++ source/scripts/ocean.gd
@@ -1,8 +1,17 @@
 extends Node
 
-var water_node = preload("res://scenes/Water.xml")
+var water_tile = load("res://scenes/Water.xml")
+var ocean_size = Vector2(2, 2)
+var tile_size = Vector2(16, 16)
 
 func _ready():
-	var instance = water_node.instance()
-	add_child(water_node)
+	#for i in range(ocean_size.x):
+	#	for j in range(ocean_size.y):
+	#		print("Stamp!")
+	#		var instance = water_tile.instance()
+	#		instance.set_pos(Vector2(i, j))
+	#		add_child(water_tile, false)
+	
+	var instance = water_tile.instance()
+	add_child(water_tile)
 	pass
--- source/scripts/world.gd
+++ source/scripts/world.gd
@@ -1,9 +1,5 @@
 extends Node
 
-var scene = load("res://scenes/Water.xml")
-
 func _ready():
-	var node = scene.instance()
-	add_child(node)
-	print("Testing")
+	
 	pass
