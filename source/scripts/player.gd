dd42520e5820ca76bf17693938eccb3bbfed7741
Unknown <b6010572@my.shu.ac.uk>
2017-12-02 11:13:36 +0000

    Player can now turn in both directions

--- source/scripts/player.gd
+++ source/scripts/player.gd
@@ -1,8 +1,31 @@
 extends Node2D
 
+var transform = get_transform()
+var speed = 0.33
+var torque = 3	# How far the player turns in degrees
+var direction = 0;
+
 func _ready():
 	set_process(true)
 	pass
 
 func _process(delta):
+	if Input.is_action_pressed("accelerate"):
+		accelerate()
 	
+	if Input.is_action_pressed("turn_left"):
+		turn_left()
+	
+	if Input.is_action_pressed("turn_right"):
+		turn_right()
+	
+	set_rotd(direction)
+
+func accelerate():
+	translate(Vector2(speed, 0))
+
+func turn_left():
+	direction += torque
+
+func turn_right():
+	direction -= torque
--- source/scripts/world.gd
+++ source/scripts/world.gd
@@ -1,5 +1,8 @@
 extends Node
 
 func _ready():
-	
+	set_process(true)
 	pass
+
+func _process(delta):
+	# TODO - game control shortcuts
