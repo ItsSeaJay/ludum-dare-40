2f22c9d5fb0c17bcbe01084beadd8d18517d255f
Unknown <b6010572@my.shu.ac.uk>
2017-12-02 09:44:49 +0000

    Having trouble instantiating the tile nodes. To the Wiki!

--- source/scripts/ocean.gd
+++ source/scripts/ocean.gd
@@ -1,7 +1,9 @@
 extends Node
 
-var water_node = preload("res://graphics/water_tile.png")
+var water_node = preload("res://scenes/Water.xml")
 
 func _ready():
-	get_tree().get_root().add_child(water_node, false)
+	var instance = water_node.instance()
+	instance.set_pos(Vector2(0, 0))
+	get_tree().get_root().get_child(0).add_child(instance)
 	pass
