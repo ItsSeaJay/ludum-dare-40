f5341de7f494e03a5ea9710269c40db9384d8a10
Unknown <b6010572@my.shu.ac.uk>
2017-12-02 10:31:42 +0000

    Set up the player scene

--- source/scripts/ocean.gd
+++ source/scripts/ocean.gd
@@ -1,13 +1,12 @@
 extends Node
 
 var water_tile = load("res://scenes/Water.xml")
-var ocean_size = Vector2(2, 2)
+var ocean_size = Vector2(32, 32)
 var tile_size = Vector2(16, 16)
 
 func _ready():
 	for x in range(ocean_size.x):
 		for y in range(ocean_size.y):
-			print("Stamp!")
 			var instance = water_tile.instance()
 			instance.set_pos(Vector2(x * tile_size.x, y * tile_size.y))
 			add_child(instance, false)
