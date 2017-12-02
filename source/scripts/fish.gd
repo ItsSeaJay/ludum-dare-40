286b8148415057790f8f0e11dc176f59330e76c8
Unknown <b6010572@my.shu.ac.uk>
2017-12-02 14:37:21 +0000

    Fish don't escape the play area any more

--- source/scripts/fish.gd
+++ source/scripts/fish.gd
@@ -1,10 +1,28 @@
 extends Node2D
 
+var ocean
+
 var speed = rand_range(8, 16)
 
 func _ready():
+	ocean = get_node("../../Ocean")
+	
 	set_process(true)
 	pass
 
 func _process(delta):
 	translate(Vector2(sin(get_rot()) * speed, cos(get_rot()) * speed) * delta)
+	screen_wrap()
+	
+func screen_wrap():
+	if (get_pos().x > ocean.get("bounds").x):
+		set_pos(Vector2(0, get_pos().y))
+	
+	if (get_pos().x < 0):
+		set_pos(Vector2(ocean.get("bounds").x, get_pos().y))
+	
+	if (get_pos().y > ocean.get("bounds").y):
+		set_pos(Vector2(get_pos().x, 0))
+	
+	if (get_pos().y < 0):
+		set_pos(Vector2(get_pos().x, ocean.get("bounds").y))
