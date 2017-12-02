78a61f6d23282863a4e3855012d2147ce9490e80
Unknown <b6010572@my.shu.ac.uk>
2017-12-02 16:52:01 +0000

    Made the fishing rod toggle between states

--- source/scripts/rod.gd
+++ source/scripts/rod.gd
@@ -3,23 +3,28 @@ extends Node2D
 var bobber = load("res://scenes/Bobber.xml")
 var bobber_instance = bobber.instance()
 var cast = false
+var pressed = false
 
 func _ready():
 	set_process(true)
 	pass
 
 func _process(delta):
-	if Input.is_action_pressed("fishing_rod") && !cast:
-		cast()
-	
-	if Input.is_action_pressed("fishing_rod") && cast:
-		reel()
+	if Input.is_action_pressed("fishing_rod") && !pressed:
+		if cast:
+			reel()
+		else:
+			cast()
+		
+		pressed = true
+	elif !Input.is_action_pressed("fishing_rod") && pressed:
+		pressed = false
 
 func cast():
 	bobber_instance.set_pos(get_global_mouse_pos())
-	get_tree().get_root().add_child(bobber_instance, false)
+	get_tree().get_root().get_node("World").add_child(bobber_instance, false)
 	cast = true
 
 func reel():
-	get_tree().get_root().remove_child(bobber_instance)
+	get_tree().get_root().get_node("World").remove_child(bobber_instance)
 	cast = false
